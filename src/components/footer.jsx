7a9d1e7c75c619045c8c6dcd4ab9846578783142
doziechuks <doziechuks1010@gmail.com>
2022-11-22 15:26:38 +0100

    I added the footer section

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import classes from './footer.module.css';
+
+const Footer = () => {
+  return ( 
+   <footer className={classes.wrapper}>
+    <p>&copy;2022 built by chuka, All rights reserved</p>
+   </footer>
+   );
+}
+
+export default Footer;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import classes from './homePage.module.css';
 import Home from '../components/home';
+import Footer from '../components/footer';
 const HomePage = () => {
   return (
     <div className={classes.wrapper}>
       <Home />
+      <Footer />
     </div>
   );
 }
